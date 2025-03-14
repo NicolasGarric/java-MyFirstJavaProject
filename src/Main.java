b8d20267e2411ceb68ab09787c6bc298109f15ef
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-03-14 12:38:48 +0100

    Learning basics

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,31 @@ public class Main {
 
         System.out.print("Test...\n");
 
+        // Integer
+        int year = 2025;
+        System.out.println("We are in " + year + " youpi :) !");
+
+        // Double
+        double price = 12.60;
+        double temperature = -12.5;
+        System.out.print("Price: ");
+        System.out.println(price + "€");
+
+        // Char
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '€';
+        System.out.println("Your grade is " + grade + symbol);
+        System.out.println("You will be payed " + price + currency);
+
+        // String
+        String myName = "Nicolas";
+        String favoriteFood = "quiche";
+        System.out.println(myName + " loves " + favoriteFood + symbol);
+
+
+
+
     }
 }
 
