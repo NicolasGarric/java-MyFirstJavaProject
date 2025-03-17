a0678373e8d36b4843cb99aa35f3332981286ddc
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-03-17 15:51:39 +0100

    Commenting old exercices

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
         // Basic tests
-
-        System.out.print("Test...\n");
-
-        // Integer
-        int year = 2025;
-        System.out.println("We are in " + year + " youpi :) !");
-
-        // Double
-        double price = 12.60;
-        double temperature = -12.5;
-        System.out.print("Price: ");
-        System.out.println(price + "€");
-
-        // Char
-        char grade = 'A';
-        char symbol = '!';
-        char currency = '€';
-        System.out.println("Your grade is " + grade + symbol);
-        System.out.println("You will be payed " + price + currency);
-
-        // String
-        String myName = "Nicolas";
-        String favoriteFood = "quiche";
-        System.out.println(myName + " loves " + favoriteFood + symbol);
-
-
-
+//
+//        System.out.print("Test...\n");
+//
+//        // Integer
+//        int year = 2025;
+//        System.out.println("We are in " + year + " youpi :) !");
+//
+//        // Double
+//        double price = 12.60;
+//        double temperature = -12.5;
+//        System.out.print("Price: ");
+//        System.out.println(price + "€");
+//
+//        // Char
+//        char grade = 'A';
+//        char symbol = '!';
+//        char currency = '€';
+//        System.out.println("Your grade is " + grade + symbol);
+//        System.out.println("You will be payed " + price + currency);
+//
+//        // String
+//        String myName = "Nicolas";
+//        String favoriteFood = "quiche";
+//        System.out.println(myName + " loves " + favoriteFood + symbol);
+//
+//
 
         // Apprentissage du scanner || entrée de texte
 
@@ -39,6 +38,9 @@ public class Main {
 
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        String age = scanner.nextLine();
 
         System.out.println("Hello " + name + " :)");
 
